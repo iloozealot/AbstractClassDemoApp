17f761a7fbf30db3c318a63f1b5628cbbeb03f58
iloozealot <iloozealot@gmail.com>
2023-12-01 15:49:32 -0600

    Add project files.

---
+++ AbstractClassDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Car car = new Car();
+
+
+
+Console.ReadLine();
+
+Book book = new Book();
+
+public abstract class InventoryItem
+{
+    public string ProductName { get; set; }
+    public int QuantityOnHand { get; set; } 
+}
+
+public abstract class Vehicle
+{
+    public string VIN { get; set; }
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public int YearManufactured { get; set; }
+}
+
+public class Car : Vehicle
+{
+    public int NumberOfWheels { get; set; } = 4;
+}
